70ffbc51cff4ea868317d3a2d2200ba83ee928af
jvillefr <82168756+jvillefr@users.noreply.github.com>
2022-03-28 16:02:26 -0400

    Add files via upload

--- sort_a.c
+++ sort_a.c
@@ -5,31 +5,35 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvillefr <jvillefr@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/24 10:04:50 by jvillefr          #+#    #+#             */
-/*   Updated: 2022/03/27 17:32:33 by jvillefr         ###   ########.fr       */
+/*   Created: 2022/03/24 11:28:20 by jvillefr          #+#    #+#             */
+/*   Updated: 2022/03/28 15:59:07 by jvillefr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 #include "../libft/libft.h"
 
-void sort_max_three(char **argv, t_stack *a, t_stack *b)
+#include "../include/push_swap.h"
+#include "../libft/libft.h"
+
+int sort_max_three(char **argv, t_stack *a, t_stack *b)
 {
-    elem *x = a->first;
-    elem *z= a->last;
+    elem *x; 
+    elem *z;
+  
+    x = a->first;
+    z = a->last;
+
     if (x->value < x->next->value)
     {
        first_case(x, z, argv, a);
     }
-    if(x->value > x->next->value && x->next->value < z->value)
+    if(x->next->value < x->value)
     {
-        rotate_a(a, argv);
+        second_case(x, z, argv, a);
     }
-    else
-    {
-        swap_a(a);
-        sort_max_three(argv, a, b);
-    }
+
+    return(0);
 }
 
 void first_case(elem *x, elem *z, char **argv, t_stack *a)
@@ -43,4 +47,55 @@ void first_case(elem *x, elem *z, char **argv, t_stack *a)
     {
         reverse_rotate_a(a, argv);
     }
+
 }
+
+void second_case(elem *x, elem *z, char **argv, t_stack *a)
+{
+    if(x->value < z->value)
+    {
+        swap_a(a);
+    }
+    if(x->next->value < z->value && z->value < x->value)
+    {
+        rotate_a(a, argv);
+    }
+    if(z->value < x->next->value)
+    {
+        swap_a(a);
+        reverse_rotate_a(a, argv);
+    }
+}
+
+bool check_sort(t_stack a)
+{
+    int j;
+    int i;
+    i = 1;
+    j = stacklen(a);
+    elem *pelem = a.first;
+    while(i < j)
+    {
+        if(pelem->value > pelem->next->value)
+        {
+            ft_putendl_fd("ko", 1);
+            return false;
+        }
+            
+        pelem = pelem->next;
+        j--;
+    }
+    ft_putendl_fd("ok", 1);
+    return true;
+}
+
+/* void print_stack(t_stack a)
+{
+    elem pelem = a.first;
+    while(pelem)
+    {
+        ft_putnbr_fd(pelem->value, 2);
+        ft_putchar_fd('\n', 2);
+        pelem = pelem->next; 
+    }
+}*/
