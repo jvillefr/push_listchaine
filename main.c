6804d2070dace26c91a49783597d2b3a2a609fa5
jvillefr <82168756+jvillefr@users.noreply.github.com>
2022-04-07 22:53:45 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jvillefr <jvillefr@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:03:42 by jvillefr          #+#    #+#             */
-/*   Updated: 2022/04/05 18:16:04 by jvillefr         ###   ########.fr       */
+/*   Updated: 2022/04/07 16:56:36 by jvillefr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int main (int argc, char **argv)
     t_stack a;
     t_stack b;
     int max;
+    int min;
     int i;
     int j;
     //argv = malloc(sizeof(argv));
@@ -41,28 +42,52 @@ int main (int argc, char **argv)
     // a = (t_stack *)malloc(sizeof(t_stack) * ft_tab_size(argv));
     //b.tab = (int *)malloc(sizeof(int) * ft_tab_size(argv));
     push_user_input(argv, &a);
-     max = find_max_nbr(a);
+    max = find_max_nbr(a);
+    min = find_min_nbr(a);
     //printf("\n\n");
+    
+
     
     while (a.first->value != max)
     {
         rotate_a(&a, argv);
     }
     push_b(argv, &a, &b);
-    fonct(argv, &a, &b);
-    roll(argv, &a, &b);
-    while (!roll)
+
+    while (a.first->value != min)
+    {
+        rotate_a(&a, argv);
+    }
+    push_b(argv, &a, &b);
+    
+    push_b(argv, &a, &b);
+    sort_max_three(argv, &b, &a);
+    
+
+    printf("\n\n");
+    printf("this is b: \n");
+    print_stack(b);
+    while (a.first && a.last)
+    {
+        if (!roll(argv, &a, &b))
+        {
+            rotate_b(&b, argv);
+        }
+    }
+    while (b.last->value != max)
     {
         rotate_b(&b, argv);
-        roll(argv, &a, &b);
     }
+    check_sort(b);
+   
     
-    while (b.first->value != max)
-    {
-        reverse_rotate_b(&b, argv);
-    }
 
-    printf(" max nbr is: %d \n", max);
+    
+    
+
+    
+    /*printf(" max nbr is: %d \n", max);
+    printf(" min nbr is: %d \n", min);
     printf("\n\n");
     printf("this is a: ");
     print_stack(a);
@@ -71,8 +96,8 @@ int main (int argc, char **argv)
     printf("----------");
     printf("\n\n");
     printf("this is b: ");
-    print_stack(b);
-    //free(argv);*/
+    print_stack(b);*/
+    //free(argv);
     return (0);
 }
 
--- sort_more.c
+++ sort_more.c
@@ -6,17 +6,15 @@
 /*   By: jvillefr <jvillefr@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:04:50 by jvillefr          #+#    #+#             */
-/*   Updated: 2022/04/05 18:17:06 by jvillefr         ###   ########.fr       */
+/*   Updated: 2022/04/07 16:28:14 by jvillefr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/push_swap.h"
 #include "../libft/libft.h"
-
+#include "../include/push_swap.h"
 
 void fonct(char **argv, t_stack *a, t_stack *b)
 {
-    push_b(argv, a, b);
     push_b(argv, a, b);
     sort_max_three(argv, b, a);
     
@@ -31,46 +29,8 @@ int roll(char **argv, t_stack *a, t_stack *b)
         reverse_rotate_b(b, argv);
         return (1);
     }
-    if(a->first->value  > b->last->value )
-    {
-        push_b(argv, a, b);
-        rotate_b(b, argv);
-        return (1);
-    }
     else
     {
         return (0);
     }
 }
-/*
-int fonct(char **argv, t_stack *a, t_stack *b)
-{
-    while(a->first)
-    {
-        if(a->first->value < b->first->value)
-        {
-            push_b(argv, a, b);
-       
-        if(a->first->value > b->first->value && a->first->value < b->first->next->value)
-        {
-            rotate_b(b, argv);
-            push_b(argv, a, b);
-        }
-        rotate_b(b, argv);
-        push_b(argv, a, b);
-        i --;
-        }
-        if(a->first->value > b->first->value && a->first->value < b->first->next->value)
-        {
-            rotate_b(a, argv);
-            push_b(argv, a, b);
-        }
-        else
-        {
-            rotate_b(a, argv);
-            push_b(argv, a, b);
-        }
-    }
-    
-    return (0);
-} */
