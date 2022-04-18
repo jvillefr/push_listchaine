6c9909c11f9773e5199aa636b0eb75600e6d7311
jvillefr <82168756+jvillefr@users.noreply.github.com>
2022-04-18 09:02:13 -0400

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jvillefr <jvillefr@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 10:04:54 by jvillefr          #+#    #+#             */
-/*   Updated: 2022/04/05 18:09:00 by jvillefr         ###   ########.fr       */
+/*   Updated: 2022/04/16 19:39:41 by jvillefr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,22 @@
 
 #include <stdbool.h>
 #include <limits.h>
+#include <stdio.h>
 
 typedef struct elem
 {
    int value;
+   int index;
+   int *index_binaire;
    struct elem *prev;
    struct elem *next;
 } elem;
+typedef struct inter
+{
+   int value;
+   struct inter *prev;
+   struct inter *next;
+} inter;
 
 typedef struct 
 {
@@ -80,6 +89,21 @@ int push_max_two(char **argv, int index, t_stack *a, t_stack *b);
 
 void fonct(char **argv, t_stack *a, t_stack *b);
 
-int roll(char **argv, t_stack *a, t_stack *b);
+int roll(char **argv, t_stack *a, t_stack *b, int max_i, int min_i);
+int find_min_nbr(t_stack a);
+int find_pindex(t_stack a, int i);
+int *bin(int a);
+void indexp(char **argv, t_stack *c);
+void view_inter(inter *p);
+int indexp_single(char **argv, t_stack *c, int val);
+void print_index(t_stack a);
+void print_index_binaire(t_stack a);
+
+int quantity_of_zero_on_bit_index(t_stack a, int index_bit);
+int quantity_of_one_on_bit_index(t_stack a, int index_bit);
+void push_zero_bit(char **argv, t_stack *a, t_stack *b, int index_bit);
+void sort_small(char **argv, t_stack *a, t_stack *b);
+void put_all_in_main_stack(char **argv, t_stack *a, t_stack *b);
+void sort_big(char **argv, t_stack *a, t_stack *b);
 
 # endif
